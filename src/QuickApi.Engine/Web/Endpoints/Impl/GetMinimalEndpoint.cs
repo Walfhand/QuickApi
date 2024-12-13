e1a203fc37a0d7ad5ea1026f49d12eefc34b6762
Sébastien <vandaele.seba@gmail.com>
2024-12-13 16:10:03 +0100

    Add getFileMinimalEndpoint

--- src/QuickApi.Engine/Web/Endpoints/Impl/GetMinimalEndpoint.cs
+++ src/QuickApi.Engine/Web/Endpoints/Impl/GetMinimalEndpoint.cs
@@ -2,6 +2,7 @@ using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Http;
 using QuickApi.Engine.Web.Cqrs;
 using QuickApi.Engine.Web.Endpoints.Enums;
+using QuickApi.Engine.Web.Models;
 
 namespace QuickApi.Engine.Web.Endpoints.Impl;
 
@@ -14,4 +15,19 @@ public abstract class GetMinimalEndpoint<TRequest, TResult>([StringSyntax("Route
     private static async Task<IResult> GetEndpoint([AsParameters] TRequest request, IMessage message,
         CancellationToken ct)
         => Results.Ok(await message.InvokeAsync<TResult>(request, ct));
+}
+
+
+public abstract class GetFileMinimalEndpoint<TRequest, TResult>(string path, params string[] policies)
+    : GetMinimalEndpoint<TRequest, TResult>(path, policies)
+    where TRequest : class
+    where TResult : FileResult
+{
+    protected override Delegate Handler => GetEndpoint;
+
+    private static async Task<IResult> GetEndpoint([AsParameters] TRequest request, IMessage message, CancellationToken ct)
+    {
+        var result = await message.InvokeAsync<TResult>(request, ct);
+        return Results.File(result.FileContent, result.ContentType, result.FileDownloadName);
+    }
 }
