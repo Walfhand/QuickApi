079ab40cdc7d6b3f8c9acedf8715109c06a1d66b
Sébastien <vandaele.seba@gmail.com>
2024-12-13 19:07:44 +0100

    add docker compose + configure open api

---
+++ src/QuickApi.Example/Config/Cors/CorsConfig.cs
@@ -0,0 +1,26 @@
+namespace QuickApi.Example.Config.Cors;
+
+public static class CorsConfig
+{
+    public static IServiceCollection AddCustomCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("default",
+                builder =>
+                {
+                    builder.WithOrigins("*")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+        });
+
+        return services;
+    }
+
+    public static WebApplication UseCustomCors(this WebApplication app)
+    {
+        app.UseCors("default");
+        return app;
+    }
+}
--- src/QuickApi.Example/Program.cs
+++ src/QuickApi.Example/Program.cs
@@ -1,18 +1,56 @@
+using System.Net;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.OpenApi.Models;
 using QuickApi.Engine.Web;
 using QuickApi.Engine.Web.Cqrs;
+using QuickApi.Example.Config.Cors;
 using QuickApi.Example.Cqrs;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddOpenApi();
 builder.Services.AddMinimalEndpoints();
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
 builder.Services.AddScoped<IMessage, MessageService>();
+builder.Services.AddCustomCors();
+builder.Services.AddHttpContextAccessor();
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    var proxyAddresses = Dns.GetHostAddresses("caddy");
+
+    foreach (var address in proxyAddresses)
+    {
+        options.KnownProxies.Add(address);
+    }
+});
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, context, cancellationToken) =>
+    {
+        var httpContextAccessor = context.ApplicationServices.GetService<IHttpContextAccessor>();
+        var httpContext = httpContextAccessor?.HttpContext;
+
+        if (httpContext == null) return Task.CompletedTask;
+        var host = httpContext.Request.Host.ToString();
+        var scheme = httpContext.Request.Scheme;
+        var baseUrl = $"{scheme}://{host}";
+        document.Servers.Clear();
+        document.Servers.Add(new OpenApiServer
+        {
+            Url = baseUrl,
+            Description = "Base URL déterminée dynamiquement"
+        });
+
+        return Task.CompletedTask;
+    });
+});
 
 var app = builder.Build();
+app.UseForwardedHeaders();
 app.MapOpenApi();
 app.MapScalarApiReference();
 
 app.UseHttpsRedirection();
 app.UseMinimalEndpoints();
+app.UseCustomCors();
 app.Run();
